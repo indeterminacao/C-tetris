dbbb9a9afcafb530c8183f9c8449b9afd1f31d6c
indeterminacao <talesvinicius2007@gmail.com>
2026-07-16 01:13:38 -0300

    refactor: split SDL initialization into dedicated helper functions

--- source_files/init.c
+++ source_files/init.c
@@ -1,28 +1,38 @@
 #include "../Gameconfs.h"
 #include "../header_files/init.h"
 
-bool sdl_initializer(struct Game *game){
-    if (SDL_Init(SDL_INIT_EVERYTHING)){
-        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-        return true;
-    }
+static bool initialize_sdl(void){
+    if (!SDL_Init(SDL_INIT_EVERYTHING)) return false;
 
+    fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
+    return true;
+}
+
+static bool create_window(struct Game *game){
     game->window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED,
                                     SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
                                     SCREEN_HEIGHT, 0);
-    if(!(game->window)){
-        fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
-        SDL_Quit(); 
-        return true;
-    }
+    if(game->window) return false;
 
+    fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+    SDL_Quit(); 
+    return true;
+}
+
+static bool create_renderer(struct Game *game){
     game->renderer = SDL_CreateRenderer(game->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if(!(game->renderer)){
-        fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
-        SDL_DestroyWindow(game->window);
-        SDL_Quit();
-        return true;
-    }
+    if(game->renderer) return false;
+
+    fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
+    SDL_DestroyWindow(game->window);
+    SDL_Quit();
+    return true;
+}
+
+bool sdl_initializer(struct Game *game){
+    if (initialize_sdl()) return true;
+    if (create_window(game)) return true;
+    if (create_renderer(game)) return true;
     
     return false;
 }
